defe0bff7a5339f554d99836501b027117b1d129
Richard Wotzlaw <r.wotzlaw@gmail.com>
2025-09-25 10:14:59 +0200

    better example points for perceptron algorithm

--- perceptron-animated.py
+++ perceptron-animated.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
 def heaviside(x):
-    return 0 if x < 0 else 1
+    return 1 if x > 0 else 0
 
 class Perceptron():
     def __init__(self, b, w0, w1, eta):
@@ -20,25 +20,29 @@ class Perceptron():
             d_b = 0
             d_w0 = 0
             d_w1 = 0
-            total_error = 0
+            total_error = 0;
             self.total_num_iterations += 1
 
-            for desired, points in training_data:
-                for x_0, x_1 in points:
+            for (y, points) in training_data:
+                for (x_0, x_1) in points:
                     z = self.b + self.w0 * x_0 + self.w1 * x_1
                     a = heaviside(z)
-                    error = desired - a
+                    error = y - a
                     d_b += self.eta * error
                     d_w0 += self.eta * error * x_0
                     d_w1 += self.eta * error * x_1
                     total_error += error ** 2
 
             if total_error == 0:
-                break
+                return
 
-            d_b /= total_num_points
-            d_w0 /= total_num_points
-            d_w1 /= total_num_points
+            if self.total_num_iterations >= 100000:
+                print(f"Giving up after {self.total_num_iterations} iterations.")
+                return
+
+            d_b = d_b / total_num_points
+            d_w0 = d_w0 / total_num_points
+            d_w1 = d_w1 / total_num_points
 
             self.b += d_b
             self.w0 += d_w0
@@ -46,9 +50,6 @@ class Perceptron():
 
             self.history.append((self.b, self.w0, self.w1))
 
-            if self.total_num_iterations >= 100:
-                print("Giving up after", self.total_num_iterations, "iterations.")
-                break
 
 def animate_perceptron(pcp, training_data):
     fig, ax = plt.subplots(figsize=(6, 6))
@@ -57,23 +58,20 @@ def animate_perceptron(pcp, training_data):
     ax.scatter(*zip(*training_data[0][1]), color='blue', label='Klasse 0')
     ax.scatter(*zip(*training_data[1][1]), color='red', label='Klasse 1')
     ax.set_xlim(-10, 10)
-    ax.set_ylim(-15, 5)
-    ax.set_xlabel('x₀')
-    ax.set_ylabel('x₁')
+    ax.set_ylim(-4, 10)
+    ax.set_xlabel('x_0')
+    ax.set_ylabel('x_1')
     ax.grid()
     ax.legend()
     line, = ax.plot([], [], color='green')
 
     def update(frame):
         b, w0, w1 = pcp.history[frame]
-        if w1 == 0:
-            x_vals = [0, 0]
-            y_vals = [-15, 5]
-        else:
-            m = -(w0 / w1)
-            n = -(b / w1)
-            x_vals = [-10, 10]
-            y_vals = [m * x + n for x in x_vals]
+        m = -(w0 / w1)
+        n = -(b / w1)
+        x_vals = [-10, 10]
+        y_vals = [m * x + n for x in x_vals]
+        
         line.set_data(x_vals, y_vals)
         ax.set_title(f"Iteration {frame+1}")
         return line,
@@ -83,8 +81,8 @@ def animate_perceptron(pcp, training_data):
 
 if __name__ == '__main__':
     classified_points = [
-        (0, [(4, -12), (-4, -7), (-6, 0)]),
-        (1, [(-1, -1), (2, -2), (3, -7)])
+        (0, [(2, 0), (-4, 3), (-5, 5), (-2, 2)]),
+        (1, [(-3, 7), (2, 5),  (5, 3), (3, 2)])
     ]
 
     pcp = Perceptron(b=3, w0=-12, w1=5, eta=0.1)
--- perceptron.py
+++ perceptron.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 
 def heaviside(x):
-    return 0 if x < 0 else 1
+    return 1 if x > 0 else 0
 
 class Perceptron():
     def __init__(self, b, w0, w1, eta):
@@ -21,8 +21,8 @@ class Perceptron():
             total_error = 0;
             self.total_num_iterations += 1
 
-            for y, points in training_data:
-                for x_0, x_1 in points:
+            for (y, points) in training_data:
+                for (x_0, x_1) in points:
                     z = self.b + self.w0 * x_0 + self.w1 * x_1
                     a = heaviside(z)
                     error = y - a
@@ -32,7 +32,11 @@ class Perceptron():
                     total_error += error ** 2
 
             if total_error == 0:
-                break
+                return
+
+            if self.total_num_iterations >= 100000:
+                print(f"Giving up after {self.total_num_iterations} iterations.")
+                return
 
             d_b = d_b / total_num_points
             d_w0 = d_w0 / total_num_points
@@ -41,27 +45,23 @@ class Perceptron():
             self.b += d_b
             self.w0 += d_w0
             self.w1 += d_w1
-            
-            if self.total_num_iterations >= 100000:
-                print("Giving up after", self.total_num_iterations, "iterations.")
-                break
 
 
 if __name__ == '__main__':
     classified_points = [
-        (0, [(4, -12), (-4, -7), (-6, 0)]),
-        (1, [(-1, -1), (2, -2), (3, -7)])
+        (0, [(2, 0), (-4, 3), (-5, 5), (-2, 2)]),
+        (1, [(-3, 7), (2, 5),  (5, 3), (3, 2)])
     ]
 
     pcp = Perceptron(3, -12, 5, 0.1)
     pcp.train(classified_points)
-
+    
     m = -(pcp.w0 / pcp.w1)
     n = -(pcp.b / pcp.w1)
 
-    print("Final weights: (b, w0, w1) = ", (pcp.b, pcp.w0, pcp.w1))
-    print("Total # of iterations:", pcp.total_num_iterations)
-    print("Linear function params (m, n) = ", (m, n))
+    print(f"Final weights: (b, w0, w1) = {(pcp.b, pcp.w0, pcp.w1)}")
+    print(f"Total # of iterations: {pcp.total_num_iterations}")
+    print(f"Linear function params (m, n) = {(m, n)}")
 
     plt.figure(1, figsize=(5, 5))
     plt.scatter(*zip(*classified_points[0][1]), color='blue', label='Klasse 0')
