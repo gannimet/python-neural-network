3e0e01a854f51eb1278269424d4c0cee88fd3bca
Richard Wotzlaw <r.wotzlaw@gmail.com>
2026-07-27 17:34:23 +0200

    implement proper sampling from softmax layer, test prediction code with endless generation loop

--- kafkai/kafkai_test_prediction.py
+++ kafkai/kafkai_test_prediction.py
@@ -3,6 +3,7 @@ import numpy
 import json
 import sys
 import os
+import time
 from kafkai_utils import model_token_to_one_hot, one_hot_to_model_token
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
@@ -25,18 +26,21 @@ nn.load_from_file(filename)
 with open("kafkai/kafkai_tokens.json", "r") as json_file:
     unique_tokens_lookup = json.load(json_file)
 
-    seed_text = "K. war telephonisch verständigt worden, daß am nächsten Sonntag eine kleine Untersuchung in seiner Angelegenheit stattfin"
+    seed_text = input("Enter prompt: ")
     seed_raw_tokens = enc.encode(seed_text)[-NUM_CONTEXT_TOKENS:]
     seed_model_tokens = [raw_token + 1 for raw_token in seed_raw_tokens]
-    known_seed_model_tokens = [0 if model_token not in unique_tokens_lookup else model_token for model_token in seed_model_tokens]
-    seed_one_hots = [model_token_to_one_hot(token, unique_tokens_lookup) for token in known_seed_model_tokens]
-    seed_input = numpy.vstack(seed_one_hots)
 
-    prediction = nn.predict(seed_input)
-    print(f"Prediction vector: {prediction}")
-
-    predicted_token = one_hot_to_model_token(prediction, unique_tokens_lookup)
-    print(f"Predicted model token: {predicted_token}")
-
-    predicted_text = "<unknown>" if predicted_token == 0 else enc.decode([predicted_token - 1])
-    print(f"Predicted text: '{predicted_text}'")
+    while True:
+        known_seed_model_tokens = [0 if model_token not in unique_tokens_lookup else model_token for model_token in seed_model_tokens]
+        seed_one_hots = [model_token_to_one_hot(token, unique_tokens_lookup) for token in known_seed_model_tokens]
+        seed_input = numpy.vstack(seed_one_hots)
+        prediction = nn.predict(seed_input)
+        predicted_token = one_hot_to_model_token(prediction, unique_tokens_lookup)
+        predicted_text = "<unknown>" if predicted_token == 0 else enc.decode([predicted_token - 1])
+        
+        print(predicted_text)
+        
+        seed_model_tokens.append(predicted_token)
+        seed_model_tokens.pop(0)
+        
+        time.sleep(1)
--- kafkai/kafkai_utils.py
+++ kafkai/kafkai_utils.py
@@ -8,6 +8,5 @@ def model_token_to_one_hot(token, unique_tokens_lookup):
     return one_hot
 
 def one_hot_to_model_token(one_hot, tokens_lookup):
-    # TODO implement sampling from distribution rather than fixed lookup
-    token_index = numpy.argmax(one_hot)
+    token_index = numpy.random.choice(len(tokens_lookup), p=one_hot.flatten())
     return tokens_lookup[token_index]
